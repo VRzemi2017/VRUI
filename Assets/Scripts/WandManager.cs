df776559cbc6caf238826a3d6003bd848c2b4ba5
sunshine <sunshine233333@gmail.com>
2017-08-02 16:47:46 +0900

    杖のモデル差し替え

---
+++ Assets/Scripts/WandManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WandManager : MonoBehaviour {
+    [SerializeField]
+    private GameObject m_Wand;
+
+
+	// Use this for initialization
+	void Start ( ) {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ( ) {
+		
+	}
+}
